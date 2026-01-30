987c10012c1f643f0f6bb1db8fa2829a948eedce
MananMaheshwari27 <66217142+MananMaheshwari27@users.noreply.github.com>
2026-01-30 18:49:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let tickets = [];
 for (let i = 1; i <= 350; i++) tickets.push(i);
-
 tickets.sort(() => Math.random() - 0.5);
 
 let prizes = [
@@ -21,16 +20,41 @@ let count = 0;
 
 function draw() {
   if (count >= 30) {
-    document.getElementById("result").innerText = "❌ DRAW COMPLETED";
+    document.getElementById("result").innerText = "DRAW COMPLETED";
     return;
   }
 
-  let ticket = tickets[count];
-  let prize = prizes[count];
+  const ball = document.getElementById("ball");
+  ball.classList.remove("pop");
+  ball.style.opacity = 0;
 
-  document.getElementById("machine").innerText = ticket;
-  document.getElementById("result").innerText =
-    `Ticket #${ticket} wins ${prize}`;
+  document.getElementById("result").innerText = "Shuffling balls...";
 
-  count++;
+  setTimeout(() => {
+    let ticket = tickets[count];
+    let prize = prizes[count];
+
+    ball.innerText = ticket;
+    ball.classList.add("pop");
+
+    document.getElementById("result").innerText =
+      `Ticket #${ticket} wins ${prize}`;
+
+    confetti();
+
+    count++;
+  }, 2000);
+}
+
+function confetti() {
+  for (let i = 0; i < 40; i++) {
+    let c = document.createElement("div");
+    c.className = "confetti";
+    c.style.left = Math.random() * 100 + "vw";
+    c.style.background =
+      ["gold", "red", "white"][Math.floor(Math.random() * 3)];
+    document.body.appendChild(c);
+
+    setTimeout(() => c.remove(), 3000);
+  }
 }
